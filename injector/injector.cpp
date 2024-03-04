0b32890867a825e57346aae210eafcb9ff314ee6
Stander-by <68646035+Stander-by@users.noreply.github.com>
2024-03-04 23:58:00 +0800

    add injector

---
+++ injector/injector.cpp
@@ -0,0 +1,41 @@
+﻿// Injector.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <cstdio>
+#include <Windows.h>
+#include <detours.h>
+#pragma comment(lib,"detours.lib")
+
+int main(int argc, char* argv[])
+{
+    if (argc == 1 || argc > 2) {
+        return -1;
+    }
+    WCHAR EXE[MAX_PATH + 1] = { 0 };
+    swprintf_s(EXE, L"%hs", argv[1]);
+    wprintf(L"%s", EXE);
+    //std::cout << "Hello World!\n";
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
+    si.cb = sizeof(STARTUPINFO);
+    WCHAR DirPath[MAX_PATH + 1];
+
+
+    wcscpy_s(DirPath, MAX_PATH, L"D:\\Project\\C++Project\\Task_SoftwareSecurity\\vvtapp\\DetoursDll3\\x64\\Debug");
+    GetCurrentDirectory(MAX_PATH, DirPath);
+    char DLLPath[MAX_PATH + 1] = "D:\\Project\\C++Project\\Task_SoftwareSecurity\\vvtapp\\DetoursDll3\\x64\\Debug\\DetoursDll3.dll";
+    //char DLLPath[MAX_PATH + 1] = "DetoursDll3.dll";
+
+    if (DetourCreateProcessWithDllEx(EXE, NULL, NULL, NULL, TRUE, CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED, NULL, DirPath, &si, &pi, DLLPath, NULL)) {
+        printf("Success Hooked!\n");
+        ResumeThread(pi.hThread);
+        WaitForSingleObject(pi.hProcess, INFINITE);
+    }
+    else {
+        return GetLastError();
+    }
+    return 0;
+}
